a5e68437cfd0011d9aa00bfdbb7dff37d5e346bb
yosifn98-cell <yosif.n98@gmail.com>
2026-07-21 16:46:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
 window.addEventListener("scroll", () => {
     document.querySelector(".navbar").style.background = "#050505";
 });
